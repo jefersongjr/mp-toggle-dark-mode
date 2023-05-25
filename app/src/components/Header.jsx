9e7b5ac1027d0716fea59aa68535bfbf25bc6a40
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-25 17:53:46 -0300

    :recycle: refactor: componetizando header

---
+++ app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Toggle from './Toggle';
+
+function Header() {
+  return (
+    <header className="w-full h-16 bg-red-500">
+      <Toggle />
+    </header>
+  );
+}
+
+export default Header;
