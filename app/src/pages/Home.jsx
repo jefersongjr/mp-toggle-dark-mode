53b96aa9c9247e4a94b3fdfcc75cb449f70aae04
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-25 17:54:03 -0300

    :recycle: refactor: componetizando o app

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,15 +1,13 @@
-import { useRef, useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import ghost from '../assets/ghost.png';
 import boo from '../assets/openEyesBoo.png';
 import backMario from '../assets/backMario.png';
 import frontMario from '../assets/fM.png';
-import blinkBlock from '../assets/blinkBlock.png';
-import block from '../assets/block.png';
 import ThemeContext from '../context/ThemeContext';
+import Header from '../components/Header';
 
 function Home() {
   const { isActive, setIsActive } = useContext(ThemeContext);
-  const toogle = useRef(null);
 
   useEffect(() => {
     const savedData = localStorage.getItem('data');
@@ -19,42 +17,12 @@ function Home() {
     }
   }, []);
 
-  const handleClick = () => {
-    const clickTarget = toogle.current;
-    if (isActive === false) {
-      clickTarget.firstChild.className = 'duration-700 translate-x-full w-1/2';
-      setIsActive(true);
-      localStorage.setItem('data', JSON.stringify(true));
-    } if (isActive === true) {
-      clickTarget.firstChild.className = 'duration-700 translate-x-0 w-1/2';
-      setIsActive(false);
-      localStorage.setItem('data', JSON.stringify(false));
-    }
-  };
-
   return (
     <div>
-      <header className="w-full h-16 bg-red-500">
-        <p className="text-blue-600">
-          Olá Toogle. tudo bem com vcs
-        </p>
-      </header>
+      <Header />
       <main
         className="flex min-h-screen"
       >
-        <button
-          id="toogle"
-          ref={ toogle }
-          type="button"
-          className="bg-emerald-400 w-32 border-4"
-          onClick={ handleClick }
-        >
-          <img
-            src={ !isActive ? block : blinkBlock }
-            alt=""
-            className="w-3/2"
-          />
-        </button>
         <div className="flex w-full h-2/5 bg-blue-900 justify-center">
           {
             (!isActive
