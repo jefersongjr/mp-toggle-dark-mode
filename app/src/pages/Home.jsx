bcc0e6329719e91195d4e56c879971d30a13af99
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-25 11:17:59 -0300

    :sparkles: feat: criando página home

---
+++ app/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div>
+      <header className="w-full h-16 bg-emerald-400">
+        <p className="text-blue-600">
+          Olá Toogle. tudo bem com vcs
+        </p>
+      </header>
+
+    </div>
+  );
+}
+
+export default Home;
