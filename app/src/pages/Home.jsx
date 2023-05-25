57e19cb5fcd3dcc13659b67bac156558382ef757
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-25 17:44:12 -0300

    :sparkles: feat: salvando status da aplicação

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,36 +1,46 @@
-import { useState, useRef } from 'react';
+import { useRef, useContext, useEffect } from 'react';
 import ghost from '../assets/ghost.png';
 import boo from '../assets/openEyesBoo.png';
 import backMario from '../assets/backMario.png';
 import frontMario from '../assets/fM.png';
 import blinkBlock from '../assets/blinkBlock.png';
 import block from '../assets/block.png';
+import ThemeContext from '../context/ThemeContext';
 
 function Home() {
-  const [isActive, setIsActive] = useState(false);
+  const { isActive, setIsActive } = useContext(ThemeContext);
   const toogle = useRef(null);
+
+  useEffect(() => {
+    const savedData = localStorage.getItem('data');
+    if (savedData) {
+      const parsedActive = JSON.parse(savedData);
+      setIsActive(parsedActive);
+    }
+  }, []);
 
   const handleClick = () => {
     const clickTarget = toogle.current;
     if (isActive === false) {
-      clickTarget.firstChild.className = 'animate-bounce';
       clickTarget.firstChild.className = 'duration-700 translate-x-full w-1/2';
       setIsActive(true);
+      localStorage.setItem('data', JSON.stringify(true));
     } if (isActive === true) {
       clickTarget.firstChild.className = 'duration-700 translate-x-0 w-1/2';
       setIsActive(false);
+      localStorage.setItem('data', JSON.stringify(false));
     }
   };
 
   return (
     <div>
-      <header className="w-full h-16 bg-emerald-400">
+      <header className="w-full h-16 bg-red-500">
         <p className="text-blue-600">
           Olá Toogle. tudo bem com vcs
         </p>
       </header>
       <main
-        className="min-h-screen"
+        className="flex min-h-screen"
       >
         <button
           id="toogle"
@@ -45,7 +55,7 @@ function Home() {
             className="w-3/2"
           />
         </button>
-        <div className="flex w-full h-2/5 bg-cyan-600 justify-center">
+        <div className="flex w-full h-2/5 bg-blue-900 justify-center">
           {
             (!isActive
               ? <img src={ ghost } alt="fantas" />
@@ -53,8 +63,8 @@ function Home() {
           }
           {
             (!isActive
-              ? <img src={ backMario } className="w-28 h-28" alt="mario" />
-              : <img src={ frontMario } alt="fantas" className="w-28 h-28" />)
+              ? <img src={ backMario } className="w-28 h-28" alt="Mario" />
+              : <img src={ frontMario } alt="Mario" className="w-28 h-28" />)
           }
         </div>
 
